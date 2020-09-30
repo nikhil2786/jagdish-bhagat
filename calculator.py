d306ffe44f95244c0ed4db3045fb16cc919368ac
Nikhil Yadav <56717314+nikhil2786@users.noreply.github.com>
2020-09-30 11:37:26 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+x=input("enter the value of a")
+z=input()
+y=input("enter the value of b")
+if(z==+):
+   print(x+y)
+if (z==-):
+    print(x-y)
+if (z==*):
+   print(x*y)
+if(z==/)
+   print(x/y)
+   
+   
